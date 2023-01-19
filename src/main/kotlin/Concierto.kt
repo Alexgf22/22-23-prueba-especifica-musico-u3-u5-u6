f7a5d56568b9d766913d3cf81cef38525b19ebbe
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-01-19 10:33:00 +0100

    importando código

---
+++ src/main/kotlin/Concierto.kt
@@ -0,0 +1,10 @@
+class Concierto {
+}
+
+
+fun main() {
+    val pianista = Pianista().apply {
+        partitura = arrayOf<Nota?>(Nota.DO, Nota.FA, Nota.FA, Nota.MI)
+    }
+    pianista.interpretar()
+}
