0d5f2686637591710e9c1674abc1de36ff5b0b26
Alexgf22 <agarcia@iesrafaelalberti.es>
2023-01-24 10:20:13 +0100

    añadiendo alumno musica al mapa del main de concierto

--- src/main/kotlin/Concierto.kt
+++ src/main/kotlin/Concierto.kt
@@ -2,10 +2,11 @@ fun main() {
     val obra = arrayOf<Nota?>(Nota.MI, Nota.MI, Nota.DO, Nota.FA)
     val interpretes = mutableMapOf<String, Interprete>(
         "Pianista" to Pianista( Piano(), obra),
-        "Violinista" to Violinista( Violin(), obra)
+        "Violinista" to Violinista( Violin(), obra),
+        "Alumno musica" to AlumnoMusica(Guitarra(),obra)
     )
 
-    repeat(2)
+    repeat(3)
     {
         interpretes.keys.random().run {
             interpretes[this]?.interpretar(obra)
